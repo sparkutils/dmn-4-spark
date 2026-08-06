0690c64829d94e03d14d81c51da7730fef0d568b
Chris Twiner <chris.twiner@gmail.com>
2026-08-06 10:37:22 +0200

    #9 - fix test case

--- server/src/test/scala/com/sparkutils/dmn/impl/test/DMN4SparkExtensionTest.scala
+++ server/src/test/scala/com/sparkutils/dmn/impl/test/DMN4SparkExtensionTest.scala
@@ -1,42 +1,47 @@
 package com.sparkutils.dmn.impl.test
 
+import com.sparkutils.dmn.impl.mock.DMNContextMock
 import com.sparkutils.dmn.{DMN, DMNConfiguration, DMNExecution, DMNModelService}
 import com.sparkutils.testing.ConnectWhenForced.someOrForcedConnect
-import org.apache.spark.sql.functions.column
+import org.apache.spark.sql.ShimUtils
+import org.apache.spark.sql.functions.{column, lit}
+import org.apache.spark.unsafe.types.UTF8String
 import org.scalatest.Matchers
-
-import scala.collection.JavaConverters.asScalaIteratorConverter
 
 class DMN4SparkExtensionTest extends SharedPureConnectTests with Matchers {
 
-  test("call function via implementation") {
+  test("call function via implementation") { evalCodeGens {
     val s = sparkSession
     import s.implicits._
 
     val dmnExecution = DMNExecution(
       Seq.empty,
-      DMNModelService("", "", None, ""),
+      DMNModelService("name", "", None, ""),
       Seq.empty,
       DMNConfiguration.empty,
     )
 
     val debug = false
 
-    for {
-      rcol <- someOrForcedConnect(DMN.dmnEval(dmnExecution, debug))
-      df = Seq(
+    val dcol = someOrForcedConnect(DMN.dmnEval(dmnExecution, debug)).
+        getOrElse(ShimUtils.callFunction("dmnEval", lit(DMNExecution.serialize(dmnExecution)), lit(debug)))
+
+    val df = Seq(
         ("foo", "bar"),
         ("foo", "baz")
       ).toDF
-      rows = df
-        .withColumn("result", rcol)
+
+    val rows = df
+        .withColumn("result", dcol)
         .select(column("result").as[String])
-        .toLocalIterator()
-        .asScala
-        .toSeq
-    } {
-      assert(rows.size === 2)
-    }
-  }
+        .collect
+
+    rows shouldBe Seq.fill(2)(
+      s"""
+         |empty,
+         |empty,
+         |${classOf[DMNContextMock].getName}""".stripMargin
+    )
+  } }
 
 }
